8c4c3e1e7ef583a881f028aef1271d166f916749
ICOnce <noaellegaard@gmail.com>
2025-08-27 11:22:15 +0200

    Did assignment

--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MyFirstGitHubClassRoomConsoleApp60;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        demo d = new demo(5, "Hello, World!");
+        for (int i = 0; i < d.LineAmount; i++) Console.WriteLine(d.LineContent);
+    }
+}
---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class demo
+    {
+        public int LineAmount {  get; set; }
+        public string LineContent { get; set; }
+
+        public demo(int lineAmount, string lineContent)
+        {
+            LineAmount = lineAmount;
+            LineContent = lineContent;
+        }
+    }
+}
